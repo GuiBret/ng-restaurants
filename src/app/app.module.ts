72f6348fd9aa76bbaf863437f1552d0dfc86058c
GuiBret <guillaume.bretzner@gmail.com>
2020-07-13 22:19:03 +0200

    Added elements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,13 @@ import { SearchComponent } from './search/search/search.component';
 import { ListComponent } from './list/list/list.component';
 import { ListElementComponent } from './list/list-element/list-element.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MapService } from './map/map.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -20,9 +27,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    LeafletModule,
+    MatListModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [MapService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
