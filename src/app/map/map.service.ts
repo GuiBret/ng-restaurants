d2fb6f0319504ae395eb79c413f11415245e9693
GuiBret <guillaume.bretzner@gmail.com>
2020-07-13 22:19:40 +0200

    Handle map init

--- src/app/map/map.service.ts
+++ src/app/map/map.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Subject, BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MapService {
-
+  currCoords: [number, number];
+  coordsToDisplayChanged = new Subject<any>();
   constructor() { }
+
+  initMap(coords: [number, number]) {
+    this.currCoords = coords;
+    
+    this.coordsToDisplayChanged.next(this.currCoords);
+  }
 }
--- src/app/map/map/map.component.ts
+++ src/app/map/map/map.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { tileLayer, latLng } from 'leaflet';
+import { MapService } from '../map.service';
 
 @Component({
   selector: 'app-map',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit {
-
-  constructor() { }
+  options: any;
+  mapCenter: [number, number];
+  constructor(private mapSvc: MapService) { }
 
   ngOnInit(): void {
+    this.options = {
+      layers: [
+        tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
+      ],
+      zoom: 15,
+      center: latLng(46.879966, -121.726909)
+    };
+
+    this.mapCenter = latLng(46.879966, -121.726909);
+    
+    this.mapSvc.coordsToDisplayChanged.subscribe(this.handleMapCoordsChanged.bind(this));
   }
+ 
+  handleMapCoordsChanged(coords: [number, number]) {
+    console.log('Cords cha')
+    this.mapCenter = latLng(coords[0], coords[1]);
+  }
+
+
 
 }
